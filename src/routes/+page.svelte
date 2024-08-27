5161ee3542a2e8094683a4f42a6b50e30e61b9f4
Adhe <adhepluk@gmail.com>
2024-08-27 13:53:50 +0700

    Add MeltUI for dialog component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { fade } from "svelte/transition";
+  import { Menu, Phone, MapPin } from "lucide-svelte";
+  import { createDialog, melt } from "@melt-ui/svelte";
+
+  const {
+    elements: { trigger, overlay, content, portalled },
+    states: { open },
+  } = createDialog({
+    forceVisible: true,
+  });
+</script>
+
+<div
+  class="flex justify-between items-center bg-white p-2 border-b border-gray-200"
+>
+  <div class="bg-gray-300 h-12 w-56 rounded"></div>
+  <div class="flex space-x-1">
+    <button
+      class="bg-white size-8 rounded flex items-center justify-center border"
+    >
+      <MapPin size="20" color="#00AA00" />
+    </button>
+    <button
+      class="bg-white size-8 rounded flex items-center justify-center border"
+    >
+      <Phone size="20" color="#00AA00" />
+    </button>
+    <button
+      use:melt={$trigger}
+      class="bg-white size-8 rounded flex items-center justify-center border"
+    >
+      <Menu size="20" color="#00AA00" />
+    </button>
+  </div>
+</div>
+
+{#if $open}
+  <div use:melt={$portalled}>
+    <div
+      use:melt={$overlay}
+      class="fixed inset-0 z-50 bg-black/50"
+      transition:fade={{ duration: 100 }}
+    ></div>
+  </div>
+{/if}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lifemate-green': '#00AA00'
+      }
+    },
   },
   plugins: [],
 }
